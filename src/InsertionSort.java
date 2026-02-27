417bf5c23cbc945ca73e448e20acf13b3eb1e3c7
Abhijeet Giri <abhijeetgiri1205@gmail.com>
2026-02-27 11:10:39 +0530

    InsertionSort.java sorting logic added -updated

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -2,7 +2,6 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {1, 2, 5, 11, 19, 12, 10, 29};
 
-
         // Print array before sorting
         System.out.println("Before sorting:");
         printArray(arr);
@@ -16,6 +15,17 @@ public class InsertionSort {
     }
 
     public static int[] insertionSort(int[] arr){
+
+        for (int i = 1; i < arr.length; i++){
+            int key = arr[i];
+            int j = i-1;
+
+            while (j>=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
 
         return arr;
     }
