ac0073c05954ce0483be99c339f218bc86178bb0
Abhijeet Giri <abhijeetgiri1205@gmail.com>
2026-02-28 22:55:31 +0530

    Linklist.java DOC strings and comments added -Updated

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,84 +1,168 @@
 package linkedlist;
-class Node{
 
-    int data;
-    Node next;
+/**
+ * Node class represents a single element in the linked list.
+ * Each node contains:
+ *  - data : the value stored in the node
+ *  - next : reference to the next node in the list
+ */
+class Node {
 
-    Node(int data){
+    int data;       // Data stored in the node
+    Node next;      // Reference to the next node
+
+    /**
+     * Constructor to initialize node with given data.
+     *
+     * @param data value to be stored in the node
+     */
+    Node(int data) {
         this.data = data;
-        next = null;
+        this.next = null; // Initially next is null
     }
 }
+
+/**
+ * LinkedList implementation using a singly linked list.
+ *
+ * Supports basic operations:
+ *  - Add at end
+ *  - Add at beginning
+ *  - Get element by index
+ *  - Delete by value
+ *  - Peek first element
+ *  - Print list
+ */
 public class LinkedList {
 
-    Node head = null;
+    private Node head = null;  // Head of the linked list
 
-    public void add(int data){
+    /**
+     * Adds a new node at the end of the list.
+     *
+     * @param data value to be added
+     */
+    public void add(int data) {
 
         Node newNode = new Node(data);
 
-        Node current = head;
+        // If list is empty, new node becomes head
         if (head == null) {
             head = newNode;
+            return;
         }
-        else {
-            while (current.next != null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
 
-    public void printValues() {
+        // Traverse to last node
         Node current = head;
-        while (current!= null) {
-            System.out.print(current.data + "->");
+        while (current.next != null) {
             current = current.next;
         }
-        System.out.println("null");
-        System.out.println();
+
+        // Attach new node at the end
+        current.next = newNode;
     }
 
+    /**
+     * Adds a new node at the beginning of the list.
+     *
+     * @param data value to be added
+     */
     public void addFirst(int data) {
+
         Node newNode = new Node(data);
 
-        if (head != null) {
-            newNode.next = head;
-        }
+        // Point new node to current head
+        newNode.next = head;
+
+        // Update head to new node
         head = newNode;
     }
 
-    public Object get(int i) {
-        int index = 0;
+    /**
+     * Returns element at given index.
+     *
+     * @param index position of element (0-based)
+     * @return data at specified index
+     * @throws IndexOutOfBoundsException if index is invalid
+     */
+    public int get(int index) {
+
+        int count = 0;
         Node current = head;
-        if (head == null){
-            return null;
-        }
-        else {
-            while (index<i){
-                current = current.next;
-                index++;
+
+        // Traverse list until index is reached
+        while (current != null) {
+
+            if (count == index) {
+                return current.data;
             }
-            return current.data;
+
+            current = current.next;
+            count++;
         }
+
+        // If index is greater than list size
+        throw new IndexOutOfBoundsException("Index: " + index);
     }
 
-    public void peek() {
-        if( head == null)
-            System.out.println("No Node present");
-        else {
-            System.out.println(head.data);
+    /**
+     * Returns the first element of the list without removing it.
+     *
+     * @return data at head node
+     * @throws IllegalStateException if list is empty
+     */
+    public int peek() {
+
+        if (head == null) {
+            throw new IllegalStateException("List is empty");
         }
+
+        return head.data;
     }
 
-    public void delete(int i) {
+    /**
+     * Deletes the first occurrence of the given value from the list.
+     *
+     * @param value element to be removed
+     */
+    public void delete(int value) {
+
+        // If list is empty, do nothing
+        if (head == null) return;
+
+        // If head contains the value
+        if (head.data == value) {
+            head = head.next;
+            return;
+        }
+
         Node current = head;
 
-        while (current.next != null && current.next.data != i){
+        // Traverse to find node before the target value
+        while (current.next != null && current.next.data != value) {
             current = current.next;
         }
-        if (current.next != null){
+
+        // If value found, remove it
+        if (current.next != null) {
             current.next = current.next.next;
         }
+    }
+
+    /**
+     * Prints all elements of the linked list.
+     * Format: data1 -> data2 -> ... -> null
+     */
+    public void printValues() {
+
+        Node current = head;
+
+        // Traverse and print each node
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+
+        System.out.println("null");
     }
 }
--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -1,21 +1,51 @@
 package linkedlist;
 
+/**
+ * Main class to demonstrate and test the LinkedList implementation.
+ *
+ * This class performs:
+ *  - Adding elements at end
+ *  - Adding element at beginning
+ *  - Getting element by index
+ *  - Peeking first element
+ *  - Deleting an element
+ *  - Printing the list
+ */
 public class Main {
+
+    /**
+     * Entry point of the program.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
+
+        // Create a new LinkedList object
         LinkedList nums = new LinkedList();
 
+        // Add elements at the end of the list
         nums.add(15);
         nums.add(5);
         nums.add(4);
 
+        // Add element at the beginning
         nums.addFirst(6);
 
-        System.out.println(nums.get(2));
-        nums.peek();
+        // Get element at index 2 (0-based index)
+        System.out.println("Element at index 2: " + nums.get(2));
 
+        // Peek first element
+        System.out.println("First element (peek): " + nums.peek());
+
+        // Print full list
+        System.out.println("\nLinked List:");
         nums.printValues();
+
+        // Delete element with value 5
         nums.delete(5);
-        nums.printValues();
 
+        // Print list after deletion
+        System.out.println("\nAfter deleting value 5:");
+        nums.printValues();
     }
 }
