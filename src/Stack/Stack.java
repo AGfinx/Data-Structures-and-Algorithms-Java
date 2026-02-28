9dc9b7d78b67a1ca194f28e6377bb29c43373f8e
Abhijeet Giri <abhijeetgiri1205@gmail.com>
2026-03-01 00:06:48 +0530

    Created constructor of Stack.java

--- src/Stack/Stack.java
+++ src/Stack/Stack.java
@@ -2,6 +2,15 @@ package Stack;
 
 public class Stack {
 
+    private int[] arr = new int[50];
+    int top;
+    int size;
+
+    Stack(){
+        top = -1;
+        size = arr.length;
+    }
+
     public void push(int i) {
 
     }
