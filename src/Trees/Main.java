7a6a6345ebfd00fd91f5dd214e5bdcc7bdcdafd7
Abhijeet Giri <abhijeetgiri1205@gmail.com>
2026-03-01 16:56:55 +0530

    BinaryTree.java main logic updated

---
+++ src/Trees/Main.java
@@ -0,0 +1,18 @@
+package Trees;
+
+public class Main {
+    public static void main(String[] args){
+
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert(2);
+        tree.insert(5);
+        tree.insert(7);
+        tree.insert(8);
+
+        tree.insert(1);
+        tree.inorder();
+        System.out.println();
+        tree.preorder();
+    }
+}
