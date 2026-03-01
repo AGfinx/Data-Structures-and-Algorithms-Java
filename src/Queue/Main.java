40d652163286972f439e0ba0ca034975ad805390
Abhijeet Giri <abhijeetgiri1205@gmail.com>
2026-03-01 09:45:35 +0530

    Queue.java main created

--- src/Queue/Main.java
+++ src/Queue/Main.java
@@ -1,4 +1,15 @@
 package Queue;
 
 public class Main {
+    public static void main(String[] args){
+
+        Queue queue = new Queue();
+
+        queue.enqueue(12);
+        queue.enqueue(1);
+        queue.enqueue(11);
+
+
+        queue.printQueue();
+    }
 }
