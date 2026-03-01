ff8fabab2b68e48e61780e4d13a021c806d91266
Abhijeet Giri <abhijeetgiri1205@gmail.com>
2026-03-01 10:05:08 +0530

    Queue.java main logic updated

--- src/Queue/Main.java
+++ src/Queue/Main.java
@@ -8,8 +8,22 @@ public class Main {
         queue.enqueue(12);
         queue.enqueue(1);
         queue.enqueue(11);
-
-
         queue.printQueue();
+
+
+        queue.dequeue();
+        queue.enqueue(14);
+        queue.enqueue(21);
+        queue.enqueue(8);
+
+
+        System.out.println(queue.peek());
+        queue.printQueue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+
     }
 }
--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -5,18 +5,47 @@ public class Queue {
     private int front=0;
     private int rear=-1;
     private int size=0;
-    private int[] arr = new int[4];
+    private final int[] arr = new int[4];
 
     public void enqueue(int data) {
-        rear++;
-        arr[rear] = data;
-        size++;
+        if (!isFull()){
+            rear = (rear + 1)%4;
+            arr[rear] = data;
+            size++;
+        }
+        else {
+            System.out.println("Queue is Full.");
+        }
     }
 
     public void printQueue(){
-        for(int i = 0; i <size; i++){
+        for(int i = front; i <size; i++){
             System.out.print(arr[i] + " ");
         }
         System.out.println();
+    }
+
+    public int dequeue() {
+        if (isEmpty()){
+            throw  new RuntimeException("Queue is Empty");
+        }
+        int data = arr[front];
+        front = (front + 1) % 4;
+        size--;
+        return data;
+    }
+
+    private boolean isFull(){
+        return size==4;
+    }
+
+    private boolean isEmpty(){
+        return size==0;
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw  new RuntimeException("Queue is Empty");
+        return arr[front];
     }
 }
